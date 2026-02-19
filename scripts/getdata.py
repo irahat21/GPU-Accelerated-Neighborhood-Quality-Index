4fa0030d6a3c17092cdd6fcf3626c0bcc7029df4
mdrhm <mdtrahman04@gmail.com>
2026-02-19 12:53:21 -0500

    Updated folder structure

--- scripts/getdata.py
+++ scripts/getdata.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-DIR = "raw_data"
+DIR = "../raw_data"
 TOKEN = "U29jcmF0YS0td2VraWNrYXNz0"
 
 DATASETS = {
@@ -10,7 +10,7 @@ DATASETS = {
     "nypd_complaints_data": "https://data.cityofnewyork.us/api/v3/views/qgea-i56i/export.csv",
     "education_quality": "https://data.cityofnewyork.us/api/v3/views/3wfy-sn5g/export.csv",
     "school_locations": "https://data.cityofnewyork.us/api/v3/views/wg9x-4ke6/export.csv",
-    "sampling_sites": "https://data.cityofnewyork.us/api/views/bkwf-xfky/files/e93e4856-95f7-48d4-b4c0-fa54989cdbfc?download=true&filename=OpenData_Distribution_Water_Quality_Sampling_Sites_Updated_2021-0618.xlsx",
+    "water_sampling_sites": "https://data.cityofnewyork.us/api/views/bkwf-xfky/files/e93e4856-95f7-48d4-b4c0-fa54989cdbfc?download=true&filename=OpenData_Distribution_Water_Quality_Sampling_Sites_Updated_2021-0618.xlsx",
 }
 
 os.makedirs(DIR, exist_ok=True)
@@ -20,12 +20,15 @@ for name, url in DATASETS.items():
     export_url = url
 
     if os.path.exists(output_file):
+        print(f"{name}.csv already exists.")
         continue
 
+    print(f"Downloading {name}.csv")
     os.system(
         f'curl -L -s -H "X-App-Token: {TOKEN}" "{export_url}" -o "{output_file}"'
     )
-
     if export_url.endswith(".xlsx"):
         df = pd.read_excel(output_file)
         df.to_csv(output_file, index=False)
+
+    print(f"{name}.csv has finished downloading")
